296c5f27d00b9f4a86e753a3ac0d9ba3435a3144
Nicolas Schneider <nicolas06062000@hotmail.com>
2018-12-08 10:12:33 +0100

    Add basic scripts template

---
+++ Assets/Scripts/Manager/EntityManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nastrond {
+    public class EntityManager : MonoBehaviour {
+
+        static EntityManager instance;
+        public static EntityManager Instance {
+            get {
+                return instance;
+            }
+        }
+
+        private void Awake() {
+            if(instance == null) {
+                instance = this;
+            } else if(instance != this) {
+                Destroy(gameObject);
+            }
+        }
+
+        List<GameObject> entities;
+
+        public List<GameObject> GetEntities() {
+            return entities;
+        }
+
+        public void RegisterAsEntities(GameObject o) {
+            if (entities == null) {
+                entities = new List<GameObject>();
+            }
+
+            if (entities.Contains(o)) {
+                return;
+            }
+
+            entities.Add(o);
+        }
+    }
+}
+
---
+++ Assets/Scripts/System/System.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nastrond {
+    public class System:MonoBehaviour {
+
+        protected List<GameObject> GetEntities() {
+            return EntityManager.Instance.GetEntities();
+        }
+    }
+}
+
