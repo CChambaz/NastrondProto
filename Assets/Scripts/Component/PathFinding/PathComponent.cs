3a3ef992181741b54965a10d9ea26ab1a43db1f3
Nicolas Schneider <nicolas06062000@hotmail.com>
2018-12-11 23:49:07 +0100

    Add basic class for storing path

---
+++ Assets/Scripts/Component/PathFinding/PathComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nastrond {
+   public class PathComponent : Component {
+       public GraphNodeComponent[] nodes;
+       public int index = 0;
+   } 
+}
+
