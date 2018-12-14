4772ec67c0fccf1ead90b0ea4e56b085d327c015
Bakakemono <robinalves1224@gmail.com>
2018-12-14 17:48:15 +0100

    Add components and systems for the dwelling

---
+++ Assets/Scripts/Component/Buildings/DwarfsSlots.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DwarfsSlots : Component
+{
+    public enum BuildingType
+    {
+        DWELLING,
+        WORKING_PLACE
+    }
+
+    public BuildingType buildingType = BuildingType.WORKING_PLACE;
+    public int maxNumberSlots = 0;
+    public int attributedDwarfsNumber = 0;
+    public int dwarfsAlreadyIn = 0;
+}
---
+++ Assets/Scripts/System/Buildings/House/DwellingConsumptionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DwellingConsumptionManager : Nastrond.System
+{
+    private FoodInventory[] foodInventorys;
+
+    private void Start()
+    {
+        Entity[] entitys = FindObjectsOfType<Entity>();
+        List<FoodInventory> tmpFoodInventorys = new List<FoodInventory>();
+
+        foreach (Entity entity in entitys)
+        {
+            FoodInventory tmpFoodInventory = entity.GetComponent<FoodInventory>();
+
+            if (tmpFoodInventory != null)
+            {
+                tmpFoodInventorys.Add(tmpFoodInventory);
+            }
+        }
+
+        foodInventorys = new FoodInventory[tmpFoodInventorys.Count];
+
+        for (int i = 0; i < tmpFoodInventorys.Count; i++)
+        {
+            foodInventorys[i] = tmpFoodInventorys[i];
+        }
+    }
+}
