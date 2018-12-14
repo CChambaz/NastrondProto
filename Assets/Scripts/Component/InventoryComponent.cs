ca8ab445a7d89f38f2e2ca129f9d7a3badd66893
SaturnCitizen <tristan.schorr@hotmail.fr>
2018-12-14 18:13:08 +0100

    first implementation ressource

---
+++ Assets/Scripts/Component/InventoryComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nastrond
+{
+    public enum RessourceType
+    {
+        COAL,
+        STONE,
+        IRON
+    }
+    public class InventoryComponent : Component
+    {
+        public int amount;
+        public int maxCapacity;
+        public RessourceType ressourceType = RessourceType.COAL;
+    }
+}
