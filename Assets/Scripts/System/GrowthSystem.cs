f749256269d3ca60cd98a3ea3cabc16477a17156
Cédric Chambaz <cdricchambaz@yahoo.fr>
2018-12-19 18:14:58 +0100

    Optimized growth system

--- Assets/Scripts/System/GrowthSystem.cs
+++ Assets/Scripts/System/GrowthSystem.cs
@@ -5,87 +5,67 @@ using Random = UnityEngine.Random;
 
 namespace Nastrond
 {
+    /* // Comment
+     * / This system is currently based on the DwarfsSlots component wich are of the type Dwelling
+     * / A problem can happen when the player destroy one of them, it will not show the exact dwarf number
+     * / Possible upgrade : base the system on the dwarves themself, should be able to be done when the spawn of them is implemented
+     */ 
     public class GrowthSystem : Nastrond.System
     {
         List<DwarfsSlots> dwarfSlots = new List<DwarfsSlots>();
-        bool dwarfSlotsHasChanged = false;
 
         int populationCapacity = 0;
         int populationCount = 0;
 
-        public float spawnDwarfTimer = 120.0f;
+        public float spawnDwarfTimerMin = 45f;
+        public float spawnDwarfTimerMax = 120f;
         public int spawnDwarfMin = 2;
         public int spawnDwarfMax = 7;
+
+        float spawnDwarfTimer = 0f;
         float lastTimeDwarfHasBeenSpawned = 0f;        
+
+        void Start()
+        {
+            Random.InitState(Random.Range(0, int.MaxValue));
+
+            spawnDwarfTimer = Random.Range(spawnDwarfTimerMin, spawnDwarfTimerMax);
+        }
 
         void Update()
         {
-            GetDwarfSlot();
-
-            if (dwarfSlotsHasChanged)
-                SetPopulationCapacity();
-
             if (Time.time > lastTimeDwarfHasBeenSpawned + spawnDwarfTimer)
-            {
-                Random.InitState(Random.Range(0, int.MaxValue));
-
                 SpawnDwarfs(Random.Range(spawnDwarfMin, spawnDwarfMax));
-            }
 
             SetPopulationCount();
         }
 
-        void GetDwarfSlot()
+        public void RegisterDwelling(DwarfsSlots ds)
         {
-            Entity[] entitys = FindObjectsOfType<Entity>();
-
-            foreach(Entity e in entitys)
-            {
-                DwarfsSlots dwarfSlot = e.GetComponent<DwarfsSlots>();
-
-                if (dwarfSlot != null && dwarfSlot.buildingType == DwarfsSlots.BuildingType.DWELLING && !dwarfSlots.Contains(dwarfSlot))
-                {
-                    dwarfSlots.Add(dwarfSlot);
-                    dwarfSlotsHasChanged = true;
-                }
-            }
-
-            Debug.Log("Count of dwarf slots found : " + dwarfSlots.Count);
+            populationCapacity += ds.maxNumberSlots;
+            dwarfSlots.Add(ds);
         }
 
-        void SetPopulationCapacity()
+        public void UnregisterDwelling(DwarfsSlots ds)
         {
-            dwarfSlotsHasChanged = false;
-
-            int tmpPopCapacity = 0;
-
-            foreach (DwarfsSlots ds in dwarfSlots)
-            {
-                tmpPopCapacity += ds.maxNumberSlots;
-            }
-
-            populationCapacity = tmpPopCapacity;
-
-            Debug.Log("Seted population capacity to " + populationCapacity);
+            populationCapacity -= ds.maxNumberSlots;
+            dwarfSlots.Remove(ds);
         }
 
         void SetPopulationCount()
         {
             int tmpPopCount = 0;
-
-            foreach(DwarfsSlots ds in dwarfSlots)
-            {
+            
+            foreach (DwarfsSlots ds in dwarfSlots)
                 tmpPopCount += ds.attributedDwarfsNumber;
-            }
 
             populationCount = tmpPopCount;
-
-            Debug.Log("Seted population count to " + populationCount);
         }
 
         void SpawnDwarfs(int number)
         {
             lastTimeDwarfHasBeenSpawned = Time.time;
+            spawnDwarfTimer = Random.Range(spawnDwarfTimerMin, spawnDwarfTimerMax);
 
             if (populationCount + number > populationCapacity)
                 number = populationCapacity - populationCount;
@@ -99,8 +79,6 @@ namespace Nastrond
                  * // TODO:
                  * // Actually creat a dwarf
                 */
-
-                Debug.Log("Dwarf n°" + i + " has been created!");
             }
         }
 
